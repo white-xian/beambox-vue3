e0a7148dcd2ea3e5b6c7d2e729959214d40e1c11
white-xian <2281091573@qq.com>
2026-04-24 14:51:00 +0800

    tabs外观更改
    由ant逻辑改为自定义导航滚动条

--- src/layouts/default/tabs/components/FoldButton.vue
+++ src/layouts/default/tabs/components/FoldButton.vue
@@ -4,34 +4,29 @@
   </span>
 </template>
 
-<script lang="ts" setup>
-  import { computed, unref } from 'vue';
-  import Icon from '@/components/Icon/Icon.vue';
+<script setup>
+import { computed, unref } from 'vue'
+import Icon from '@/components/Icon/Icon.vue'
+import { useDesign } from '@/hooks/web/useDesign'
+import { useHeaderSetting } from '@/hooks/setting/useHeaderSetting'
+import { useMenuSetting } from '@/hooks/setting/useMenuSetting'
+import { triggerWindowResize } from '@/utils/event'
 
-  import { useDesign } from '@/hooks/web/useDesign';
-  import { useHeaderSetting } from '@/hooks/setting/useHeaderSetting';
-  import { useMenuSetting } from '@/hooks/setting/useMenuSetting';
-  import { triggerWindowResize } from '@/utils/event';
+defineOptions({ name: 'FoldButton' })
 
-  defineOptions({ name: 'FoldButton' });
+const { prefixCls } = useDesign('multiple-tabs-content')
+const { getShowMenu, setMenuSetting } = useMenuSetting()
+const { getShowHeader, setHeaderSetting } = useHeaderSetting()
+const getIsUnFold = computed(() => !unref(getShowMenu) && !unref(getShowHeader))
+const getIcon = computed(() => (unref(getIsUnFold) ? 'codicon:screen-normal' : 'codicon:screen-full'))
 
-  const { prefixCls } = useDesign('multiple-tabs-content');
-  const { getShowMenu, setMenuSetting } = useMenuSetting();
-  const { getShowHeader, setHeaderSetting } = useHeaderSetting();
-
-  const getIsUnFold = computed(() => !unref(getShowMenu) && !unref(getShowHeader));
-
-  const getIcon = computed(() =>
-    unref(getIsUnFold) ? 'codicon:screen-normal' : 'codicon:screen-full',
-  );
-
-  function handleFold() {
-    const isUnFold = unref(getIsUnFold);
-    setMenuSetting({
-      show: isUnFold,
-      hidden: !isUnFold,
-    });
-    setHeaderSetting({ show: isUnFold });
-    triggerWindowResize();
-  }
+function handleFold() {
+  const isUnFold = unref(getIsUnFold)
+  setMenuSetting({
+    show: isUnFold,
+    hidden: !isUnFold,
+  })
+  setHeaderSetting({ show: isUnFold })
+  triggerWindowResize()
+}
 </script>
--- src/layouts/default/tabs/components/TabRedo.vue
+++ src/layouts/default/tabs/components/TabRedo.vue
@@ -1,34 +1,42 @@
 <template>
   <span :class="`${prefixCls}__extra-redo`" @click="handleRedo">
-    <RedoOutlined :spin="loading" />
+    <Icon icon="ep:refresh-right" :class="{ 'is-spinning': loading }" />
   </span>
 </template>
 
-<script lang="ts" setup>
-  import { ref } from 'vue';
-  import { RedoOutlined } from '@ant-design/icons-vue';
-  import { useDesign } from '@/hooks/web/useDesign';
-  import { useTabs } from '@/hooks/web/useTabs';
+<script setup>
+import { ref } from 'vue'
+import Icon from '@/components/Icon/Icon.vue'
+import { useDesign } from '@/hooks/web/useDesign'
+import { useTabs } from '@/hooks/web/useTabs'
 
-  defineOptions({ name: 'TabRedo' });
+defineOptions({ name: 'TabRedo' })
 
-  const loading = ref(false);
+const loading = ref(false)
+const { prefixCls } = useDesign('multiple-tabs-content')
+const { refreshPage } = useTabs()
 
-  const { prefixCls } = useDesign('multiple-tabs-content');
-  const { refreshPage } = useTabs();
-
-  async function handleRedo() {
-    loading.value = true;
-    await refreshPage();
-    setTimeout(() => {
-      loading.value = false;
-      // Animation execution time
-    }, 1200);
-  }
+async function handleRedo() {
+  loading.value = true
+  await refreshPage()
+  setTimeout(() => {
+    loading.value = false
+  }, 1200)
+}
 </script>
 
 <style lang="less" scoped>
-  span.anticon-redo {
-    vertical-align: baseline !important;
+.is-spinning {
+  animation: tab-redo-spin 0.8s linear infinite;
+}
+
+@keyframes tab-redo-spin {
+  from {
+    transform: rotate(0deg);
   }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
 </style>
