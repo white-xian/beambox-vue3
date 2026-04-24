5343104da98c3c3a34003fc4aa3e86852db24a84
white-xian <2281091573@qq.com>
2026-04-24 14:45:05 +0800

    挂载element plus

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import 'uno.css';
 import '@/design/index.less';
 import '@/components/VxeTable/src/css/index.scss';
 import 'ant-design-vue/dist/reset.css';
+import 'element-plus/dist/index.css';
 import 'virtual:svg-icons-register';
 
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
 
 import { registerGlobComp } from '@/components/registerGlobComp';
 import { setupGlobDirectives } from '@/directives';
@@ -19,6 +22,7 @@ import App from './App.vue';
 async function bootstrap() {
   const app = createApp(App);
 
+  app.use(ElementPlus, { locale: zhCn });
   setupStore(app);
   initAppConfigStore();
   registerGlobComp(app);
